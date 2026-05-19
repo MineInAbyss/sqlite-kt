57619627b560895b19f2bf4d9b4c6caf9ed4d11b
Danielle Voznyy <dan.voznyy@gmail.com>
2026-05-19 00:47:01 -0400

    feat: Add helpers for counting changes from a statement

--- src/commonMain/kotlin/me/dvyy/sqlite/statement/SelectStatement.kt
+++ src/commonMain/kotlin/me/dvyy/sqlite/statement/SelectStatement.kt
@@ -13,6 +13,9 @@ class SelectStatement<R>(
         block(context)
     }
 
+    /**
+     * Run on each result row
+     */
     context(tx: Transaction)
     inline fun forEach(
         statement: NamedColumnSqliteStatement.(R) -> Unit,
@@ -24,6 +27,9 @@ class SelectStatement<R>(
         }
     }
 
+    /**
+     * Query each result row, building a list by mapping each.
+     */
     context(tx: Transaction)
     inline fun <T> map(
         statement: NamedColumnSqliteStatement.(R) -> T,
@@ -33,6 +39,9 @@ class SelectStatement<R>(
         }
     }
 
+    /**
+     * Query the first result row, or error if none were returned.
+     */
     context(tx: Transaction)
     inline fun <T> first(
         statement: NamedColumnSqliteStatement.(R) -> T,
@@ -41,11 +50,32 @@ class SelectStatement<R>(
         else error("Tried getting first row from empty result set")
     }
 
+    /**
+     * Query the first result row, or return null if none were returned.
+     */
     context(tx: Transaction)
     inline fun <T> firstOrNull(
         statement: NamedColumnSqliteStatement.(R) -> T,
     ): T? = prepare {
         if (step()) statement(context)
         else null
+    }
+
+    /**
+     * @return Whether any rows were changed as a result of this statement.
+     */
+    context(tx: Transaction)
+    fun anyChanged(): Boolean {
+        prepare { step() }
+        return countChanges() != 0
+    }
+
+    /**
+     * @return How many row changes occurred as a result of this statement.
+     */
+    context(tx: Transaction)
+    fun countChanges(): Int {
+        prepare { step() }
+        return tx.select("SELECT changes()").first { getInt(0) }
     }
 }
